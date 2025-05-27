e32f2006c80745b84cad47b75fa4c4747c994b58
Zeryab Alam <zeryabalam272@gmail.com>
2025-05-27 12:09:19 +0200

    Add GCN reproduction data for self experiments

---
+++ experiments/reproduction_data_gcn.py
@@ -0,0 +1,74 @@
+gcn_data = {
+    "CSBM": {
+        "Sample Wise": {
+            0.05: {
+                "accuracy_test": 0.8666666746139526,
+                "accuracy_trn": 1.0,
+                "accuracy_cert_pois_robust": 0.7888888888888889
+            },
+            0.1: {
+                "accuracy_test": 0.8666666746139526,
+                "accuracy_trn": 1.0,
+                "accuracy_cert_pois_robust": 0.42777777777777776
+            },
+            0.15: {
+                "accuracy_test": 0.8666666746139526,
+                "accuracy_trn": 1.0,
+                "accuracy_cert_pois_robust": 0.027777777777777776
+            }
+        },
+        "Collective": {
+            0.05: {
+                "accuracy_test": 0.8666666746139526,
+                "accuracy_trn": 1.0,
+                "accuracy_cert_pois_robust": 0.8888888888888888
+            },
+            0.1: {
+                "accuracy_test": 0.8666666746139526,
+                "accuracy_trn": 1.0,
+                "accuracy_cert_pois_robust": 0.7444444444444445
+            },
+            0.15: {
+                "accuracy_test": 0.8666666746139526,
+                "accuracy_trn": 1.0,
+                "accuracy_cert_pois_robust": 0.5055555555555555
+            }
+        }
+    },
+    "CBA": {
+        "Sample Wise": {
+            0.05: {
+                "accuracy_test": 0.8388888835906982,
+                "accuracy_trn": 1.0,
+                "accuracy_cert_pois_robust": 0.7444444444444445
+            },
+            0.1: {
+                "accuracy_test": 0.8388888835906982,
+                "accuracy_trn": 1.0,
+                "accuracy_cert_pois_robust": 0.4444444444444444
+            },
+            0.15: {
+                "accuracy_test": 0.8388888835906982,
+                "accuracy_trn": 1.0,
+                "accuracy_cert_pois_robust": 0.1
+            }
+        },
+        "Collective": {
+            0.05: {
+                "accuracy_test": 0.8388888835906982,
+                "accuracy_trn": 1.0,
+                "accuracy_cert_pois_robust": 0.8944444444444445
+            },
+            0.1: {
+                "accuracy_test": 0.8388888835906982,
+                "accuracy_trn": 1.0,
+                "accuracy_cert_pois_robust": 0.7444444444444445
+            },
+            0.15: {
+                "accuracy_test": 0.8388888835906982,
+                "accuracy_trn": 1.0,
+                "accuracy_cert_pois_robust": 0.5722222222222224
+            }
+        }
+    }
+}
